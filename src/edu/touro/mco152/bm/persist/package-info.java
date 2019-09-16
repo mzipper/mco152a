381327027e74f5c6c7114eb70a4a429823091111
Chaim Weinberg <chaimw13@gmail.com>
2019-09-16 00:20:39 -0400

    Changed persist package-info documentation

--- src/edu/touro/mco152/bm/persist/package-info.java
+++ src/edu/touro/mco152/bm/persist/package-info.java
@@ -3,7 +3,8 @@
  * data objects (entities) to a permanent store.
  * It uses the Java persistence interface, and
  * typically is configured to use the Apache DERBY RDBMS
- * 
+ * The related classes use Java's Entity Manager as well as the
+ * Serializable class to store data.
  */
 
 package edu.touro.mco152.bm.persist;
